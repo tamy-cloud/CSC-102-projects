31667f660543c9be7a6d44e1cdd55b2190eef1bc
tamy-cloud <tamaracolleges1@gmail.com>
2026-04-29 10:43:35 -0400

    Update bomb_phases.py

--- bomb_phases.py
+++ bomb_phases.py
@@ -1,7 +1,7 @@
 #################################
 # CSC 102 Defuse the Bomb Project
 # GUI and Phase class definitions
-# Team: 
+# Team: Tamara and her Minions
 #################################
 
 # import the configs
