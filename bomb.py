937dce04c782c8655680dff3b2904661affd77fb
tamy-cloud <tamaracolleges1@gmail.com>
2026-05-02 21:06:27 -0400

    everything is wrong!!!!!
    OMG I think I just almost killed this becasue everything stoped working and its late and I'm hungry for like a smothie or something and it wasn't working but hopefully this one deletion will fix it.

--- bomb.py
+++ bomb.py
@@ -119,7 +119,6 @@ def check_phases():
             toggles._failed = False
 #I think we change to yarn balls somewhere around here
     # note the strikes on the GUI
-    gui._lstrikes["text"] = f"Strikes left: {strikes_left}"
     # too many strikes -> explode!
     if (strikes_left == 0):
         # turn off the bomb and render the conclusion GUI
